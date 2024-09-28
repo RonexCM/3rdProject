98743e34a3dbbe268e45f52975e92026a0b1cf16
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2024-09-28 15:31:00 +0545

    Add files via upload

---
+++ database/housepredict.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 28, 2024 at 11:44 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `housepredict`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `predictions`
+--
+
+CREATE TABLE `predictions` (
+  `user_id` int(6) NOT NULL,
+  `aana` int(6) NOT NULL,
+  `bedroom` int(6) NOT NULL,
+  `bathroom` int(6) NOT NULL,
+  `floor` int(6) NOT NULL,
+  `road` int(6) NOT NULL,
+  `location` varchar(256) NOT NULL,
+  `price` varchar(256) NOT NULL,
+  `model_type` varchar(256) NOT NULL,
+  `date` datetime(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(7, 'Ronex', 'ronexshakya28@gmail.com', '$2y$10$oNbHdtZv1D5qpqJABylkFO7Y8l/Stqsb.KdGkzitl4Lqwivpy/eHK'),
+(8, 'Tenzo', 'tenzo@gmail.com', '$2y$10$.fZLY6XKzM9bj0qOxyHUGugFsuKQjSUpi7uZUEfOktkkW029QagVS'),
+(9, 'gre_roku', 'test@test.com', '$2y$10$pgOQfiLTvIvjnr5TE5EzreIEArH21E.XsV5/zsVXGl3Y2x97wCYq6'),
+(10, 'test', 'aman@gmail.com', '$2y$10$Q2vE1VWXnqQ/S2BWKvXkBuyHGIyzAa1ZM8sv2ql5mftjyGaZtzjOm');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `predictions`
+--
+ALTER TABLE `predictions`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
